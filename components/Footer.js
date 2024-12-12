081eeec0c728d4471a4ce595cc21bd60f94953ee
Leonard <leonard.momber@posteo.de>
2024-12-12 22:52:54 +0100

    delete use effect footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,34 +1,10 @@
 import styled from "styled-components";
-import { useState, useEffect } from "react";
 
 import Form from "./Form";
 
 export default function Footer({ addProduct }) {
-  const [footerBottom, setFooterBottom] = useState(0);
-
-  useEffect(() => {
-    const handleResize = () => {
-      const isKeyboardOpen =
-        window.innerHeight < document.documentElement.clientHeight;
-      if (isKeyboardOpen) {
-        setFooterBottom(
-          document.documentElement.clientHeight - window.innerHeight
-        );
-      } else {
-        setFooterBottom(0);
-      }
-    };
-
-    window.addEventListener("resize", handleResize);
-    handleResize(); // Initial check
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
   return (
-    <StyledFooter style={{ bottom: `${footerBottom}px` }}>
+    <StyledFooter>
       <Form addProduct={addProduct} />
     </StyledFooter>
   );
@@ -42,5 +18,4 @@ const StyledFooter = styled.div`
   background-color: #264653;
   padding: 1rem;
   text-align: center;
-  transition: bottom 0.3s ease-in-out;
 `;
